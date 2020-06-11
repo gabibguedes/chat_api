9b7317210dcf58d7dc37e1430316403721c7fb19
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2020-06-11 17:05:57 -0300

    Start Django Rest API
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

---
+++ api/views.py
@@ -0,0 +1,8 @@
+#views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response('HELLO WORLD! from Django.')
