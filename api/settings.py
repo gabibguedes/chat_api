f69aac16df3405a2f8bbd4d003ccd348b2349207
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2020-06-13 16:31:18 -0300

    Fix cors
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- api/settings.py
+++ api/settings.py
@@ -51,8 +51,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -143,3 +143,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     )
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
